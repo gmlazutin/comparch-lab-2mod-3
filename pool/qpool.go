2d77318795cbc9afb17947724cf90095c72ffb08
gmlazutin <l4zutin.gleb@yandex.com>
2026-02-26 08:28:29 +0300

    avoid race condition

--- pool/qpool.go
+++ pool/qpool.go
@@ -13,6 +13,7 @@ type QPool[T any] struct {
 	cancel context.CancelFunc
 	wg     sync.WaitGroup
 	once   sync.Once
+	pushWg sync.WaitGroup
 }
 
 func NewQPool[T any](workers int, routine ProcessRoutine[T]) *QPool[T] {
@@ -36,22 +37,22 @@ func NewQPool[T any](workers int, routine ProcessRoutine[T]) *QPool[T] {
 	return ip
 }
 
-func (ip *QPool[T]) PushContext(ctx context.Context, task T) error {
+func (ip *QPool[T]) Push(task T) error {
+	ip.pushWg.Add(1)
+	defer ip.pushWg.Done()
+
 	select {
-	case <-ctx.Done():
-		return ctx.Err()
+	case <-ip.ctx.Done():
+		return ip.ctx.Err()
 	case ip.tasks <- task:
 		return nil
 	}
 }
 
-func (ip *QPool[T]) Push(task T) error {
-	return ip.PushContext(context.Background(), task)
-}
-
 func (ip *QPool[T]) Stop() {
 	ip.once.Do(func() {
 		ip.cancel()
+		ip.pushWg.Wait()
 		close(ip.tasks)
 		ip.wg.Wait()
 	})
